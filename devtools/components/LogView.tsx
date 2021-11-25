4c7b17deebaf89f3c38e606e76962ac0053120bf
Guido D'Orsi <gu.dorsi@gmail.com>
2021-11-25 15:57:34 +0100

    feat(devtools): adds an input to filter atoms by key

--- devtools/components/LogView.tsx
+++ devtools/components/LogView.tsx
@@ -1,5 +1,5 @@
 import { css, keyframes } from '@emotion/react';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { ATOM_LOG } from '../types';
 import { ObjectViewer } from './ObjectViewer';
 
@@ -21,6 +21,12 @@ const ContainerStyle = css({
     gap: '6px',
 });
 
+const InputStyle = css({
+    borderWidth: 0,
+    width: '100%',
+    padding: '14px 6px',
+});
+
 const RowStyle = css({
     background: '#fff',
     border: '1px solid #ddd',
@@ -30,15 +36,6 @@ const MainRowStyle = css({
     fontWeight: '600' as 'bold',
     padding: '4px 0 0 8px',
     animation: `${highlight} 0.6s`,
-});
-
-const ButtonToggleStyle = css({
-    display: 'inline-block',
-    marginRight: '2px',
-    fontWeight: '400' as 'bolder',
-    fontSize: '0.8em',
-    float: 'right',
-    padding: '1px',
 });
 
 const NewLabelStyle = css({
@@ -59,76 +56,57 @@ const UpdateLabelStyle = css({
 });
 
 export function LogView({ data }: { data: ATOM_LOG }) {
-    const [hiddenKeys, setHiddenKeys] = useState<string[]>(() => {
-        const saved = localStorage.getItem('hiddenKeys');
-
-        if (saved) return JSON.parse(saved);
-
-        return [];
-    });
-
-    function toggleKey(key: string) {
-        setHiddenKeys((value) =>
-            value.includes(key)
-                ? value.filter((v) => v !== key)
-                : value.concat(key)
-        );
-    }
-
-    useEffect(() => {
-        localStorage.setItem('hiddenKeys', JSON.stringify(hiddenKeys));
-    }, [hiddenKeys]);
+    const [filter, setFilter] = useState('');
 
     return data.length ? (
-        <div css={ContainerStyle}>
-            {data.map(({ key, value, uid, type }, i, list) => {
-                const sameAsPrev = list[i - 1] && list[i - 1].key === key;
-                const sameAsNext = list[i + 1] && list[i + 1].key === key;
+        <>
+            <input
+                css={InputStyle}
+                type="text"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+                placeholder="Type here to filter by key"
+            />
+            <div css={ContainerStyle}>
+                {data.map(({ key, value, uid, type }, i, list) => {
+                    const sameAsPrev = list[i - 1] && list[i - 1].key === key;
+                    const sameAsNext = list[i + 1] && list[i + 1].key === key;
 
-                let updates = 1;
+                    let updates = 1;
 
-                if (sameAsPrev && !sameAsNext) {
-                    let j = i - 1;
+                    if (sameAsPrev && !sameAsNext) {
+                        let j = i - 1;
 
-                    while (list[j] && list[j].key === key) {
-                        updates++;
-                        j--;
+                        while (list[j] && list[j].key === key) {
+                            updates++;
+                            j--;
+                        }
                     }
-                }
 
-                const visible = !hiddenKeys.includes(key);
+                    if (filter && !key.startsWith(filter)) return null;
 
-                if (sameAsNext && !visible) return null;
-
-                return (
-                    <div css={RowStyle} key={uid}>
-                        {!sameAsNext && (
-                            <div css={MainRowStyle}>
-                                {key}
-                                <button
-                                    css={ButtonToggleStyle}
-                                    onClick={() => toggleKey(key)}
-                                >
-                                    {visible ? 'hide' : 'show'}
-                                </button>
-                                {type === 'NEW_ATOM' ? (
-                                    <span css={NewLabelStyle}>NEW</span>
-                                ) : null}
-                                {updates > 1 ? (
-                                    <span css={UpdateLabelStyle}>
-                                        {updates} updates
-                                    </span>
-                                ) : null}
-                            </div>
-                        )}
-                        {visible ? (
+                    return (
+                        <div css={RowStyle} key={uid}>
+                            {!sameAsNext && (
+                                <div css={MainRowStyle}>
+                                    {key}
+                                    {type === 'NEW_ATOM' ? (
+                                        <span css={NewLabelStyle}>NEW</span>
+                                    ) : null}
+                                    {updates > 1 ? (
+                                        <span css={UpdateLabelStyle}>
+                                            {updates} updates
+                                        </span>
+                                    ) : null}
+                                </div>
+                            )}
                             <div>
                                 <ObjectViewer data={value} />
                             </div>
-                        ) : null}
-                    </div>
-                );
-            })}
-        </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
     ) : null;
 }
